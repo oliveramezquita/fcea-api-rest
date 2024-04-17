c1368c7bd30c832132c261e3f5dfcd7dbe6e81ce
Oliver Amezquita Morales <varilla24@gmail.com>
2024-04-16 20:07:19 -0600

    Use atomic in data process

--- formsapp/views.py
+++ formsapp/views.py
@@ -1,12 +1,14 @@
 from rest_framework.views import APIView
 from formsapp.use_cases.data_proccess_use_case import DataProccessUseCase
 from formsapp.use_cases.get_raw_data_use_case import GetRawDataUseCase
+from django.db.transaction import atomic
 
 
 class DataProccessView(APIView):
     def post(self, request):
-        use_case = DataProccessUseCase(raw_data=request.data)
-        return use_case.proccess()
+        with atomic():
+            use_case = DataProccessUseCase(raw_data=request.data)
+            return use_case.proccess()
 
 
 class RawDataView(APIView):

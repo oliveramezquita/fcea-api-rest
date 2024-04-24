5d583b8d46e4a5692476b5e043229c3318565bc3
Oliver Amezquita Morales <varilla24@gmail.com>
2024-04-24 17:50:14 -0600

    Folders for uploads have been activated

--- fcea_monitoreo/settings.py
+++ fcea_monitoreo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import sentry_sdk
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,3 +175,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 ADMIN_URL = config('ADMIN_URL')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'

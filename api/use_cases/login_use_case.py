a013057cd4ced492e64d95837ae6808c3ef05746
Oliver Amezquita Morales <varilla24@gmail.com>
2025-03-19 21:03:45 -0600

    New log info in authentication proccess

--- api/use_cases/login_use_case.py
+++ api/use_cases/login_use_case.py
@@ -24,8 +24,8 @@ class LoginUseCase:
         self.validate_params()
         if self.authentication():
             auth_data = self.auth_data()
-            print(f'auth_data: {auth_data}')
             dump = json.dumps(auth_data)
+            print(f'dump: {dump}')
             return HttpResponse(dump, content_type='application/json')
         return bad_request('Acceso incorrecto', HTTP_403_FORBIDDEN)
 
